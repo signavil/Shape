649532297fb81058430878492ec68d2831fbe533
Lenovo <2297598918@qq.com>
2021-05-11 22:47:10 +0800

    Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -1,6 +1,5 @@
 abstract class Shape {
     public abstract double area();
-    public abstract double L();
 }
 class Circle extends Shape {
     private double r;
@@ -20,9 +19,6 @@ class Rectangle extends Shape{
     public Rectangle(double s1, double s2) {
         a=s1; b=s2;
     }
-    public double L() {
-        return 2*(a+b);
-    }
     public double area(){
         return a*b;
     }
@@ -32,9 +28,7 @@ class result {
 {
     Shape s1=new Circle(10);
     System.out.println("圆形面积为:"+s1.area());
-    System.out.println("圆形周长为:"+s1.L());
     Shape s2=new Rectangle(2, 3);
     System.out.println("长方形面积为:"+s2.area());
-    System.out.println("长方形周长为:"+s2.L());
 }
 }
